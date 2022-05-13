e16d31d03e2a73bb7449b54663a09f54885eb1d6
FawziLinggo <fawzilinggo@gmail.com>
2022-05-13 10:51:14 +0700

    Belajar Record

---
+++ src/main/java/fawzi/linggo/app/RecordApp.java
@@ -0,0 +1,11 @@
+package fawzi.linggo.app;
+
+import fawzi.linggo.record.Record;
+
+public class RecordApp {
+    public static void main(String[] args) {
+        Record record = new Record("fawzi", "321123");
+//        System.out.println(record.password());
+//        System.out.println(record);
+    }
+}
---
+++ src/main/java/fawzi/linggo/record/Record.java
@@ -0,0 +1,8 @@
+package fawzi.linggo.record;
+
+public record Record(String username, String password) {
+
+    public Record{
+        System.out.println("yamette kudasai");
+    }
+}
